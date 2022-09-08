aa3ee86dd4713e2f51a58da55c57ea4f1b664e2d
ronicleirps <roniclei@outlook.com>
2022-09-08 15:35:12 -0300

    criação do projeto

---
+++ android/app/src/main/kotlin/com/example/pomodo_timer_em_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pomodo_timer_em_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:pomodo_timer_em_flutter/Home.dart';
+
+void main() {
+  runApp(const Home());
+}
+
